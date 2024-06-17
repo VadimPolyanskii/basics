838f03911cbada60b29ec8f58a5ae5e0a0ef87f9
Vad Pol <work.arb19@gmail.com>
2024-06-17 19:51:50 +0300

    Modul 1

---
+++ variables.py
@@ -0,0 +1,17 @@
+
+
+name = "Alex"                       # Сотрудник
+female = False                      # Женский ли пол?
+age = 30                            # Возраст
+best = True                         # Лучший или нет?
+salary = 700                      # Оклад в долларах
+job_title = "salesman"              # Должность: продавец
+experience = 5.7                    # опыт работы: 5 лет 7 мес.
+
+print(type(name))
+print(type(female))
+print(type(age))
+print(type(best))
+print(type(salary))
+print(type(job_title))
+print(type(experience))
